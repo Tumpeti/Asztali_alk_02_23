ec9c43589e859115d74172f6a408009efc06261b
Tumpeti <petertumpek20@gmail.com>
2023-02-23 18:27:46 +0100

    kiírás működő verzió

--- MetodusokGyak1.java
+++ MetodusokGyak1.java
@@ -11,11 +11,11 @@ package metodusok;
 public class MetodusokGyak1 {
     public static void main(String[] args){
         int osszeg = elso10SzamOsszege();
-        String kimenet ="Az elsõ 10 szám összege: " + osszeg + "\n";
+        String kimenet ="A két szám összege: " + osszeg + "\n";
         kiir(kimenet);
         
         int szam1 = 7, szam2 =4;
-        kiir("%d + %d = %d\n".formatted(osszead(szam1, szam2)))
+        kiir("%d + %d = %d\n".formatted(szam1, szam2, osszeg));
     }
     
     private static int elso10SzamOsszege(){
@@ -33,4 +33,5 @@ public class MetodusokGyak1 {
  
 private static void kiir(String szoveg) {
     System.out.print(szoveg);
+}
 }
