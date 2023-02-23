6f8035cdf375d18bf5664d062af705e166ea9448
Tumpeti <petertumpek20@gmail.com>
2023-02-23 17:38:12 +0100

    fájl létrehozása

---
+++ MetodusokGyak1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metodusok;
+
+/**
+ *
+ * @author tumpi
+ */
+public class MetodusokGyak1 {
+    public static void main(String[] args){
+        
+    }
+    
+    private static void elso10SZamOsszege(){
+        //összead
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++)
+            osszeg += i;
+        return osszeg;
+    }
+    private class main(String[] args) {
+    System.out.println(myMethod(3));
+  }
+}
